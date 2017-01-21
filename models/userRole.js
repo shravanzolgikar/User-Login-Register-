89bc26652973709d156adb38e82f4ef75b687dbe
shravan <shravan.olgikar13@gmail.com>
2017-01-21 17:44:02 +0530

    UserRole Model Modified

--- models/userRole.js
+++ models/userRole.js
@@ -1,8 +1,6 @@
 exports.userRoles =
     {
         Admin: 'ADMIN',
-        Organizer: 'ORGANIZER',
-        TeamCap: 'TEAM_CAPTAIN',
-        TeamPlayer: 'TEAM_PLAYER',
-        Donor: 'DONOR'
+        Organizer: 'ORGANIZER'
+
     };
